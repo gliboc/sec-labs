625a811b8964bbfd6db6816132328c590389419f
gduboc <guillaume.duboc@ens-lyon.fr>
2018-10-06 16:26:45 +0200

    step7 add basic code generation

--- lab_1/_code/step7/ocaml/ast.ml
+++ lab_1/_code/step7/ocaml/ast.ml
@@ -1,12 +1,13 @@
-type actuator = string * int
+type actuator = {name: string; pin: int}
 type actuators = actuator list
 
 type pin = int
+type signal = Low | High
 
-type action = string * pin
+type action = {actuator: actuator; value: signal; values: signal list}
 type actions = action list
 
-type state = Init of string * actions * state
-type states = state list
+type state = {name: string; actions: actions; next: state}
+type states = state list 
  
-type term = App of string * actuators * states
+type term = App of string * actuators * state * states
--- lab_1/_code/step7/ocaml/generator.ml
+++ lab_1/_code/step7/ocaml/generator.ml
@@ -5,9 +5,10 @@ let code = Queue.create ()
 let c s = Queue.add s code
 
 
-let rec gen t = 
+let rec gen : term -> unit = fun t ->
     match t with
-    | App (name, actuators, actions) -> 
+    | App (name, actuators, initial, states) -> 
+
         c("// C code generated from an object model");
 		c(Printf.sprintf "// Application name: %s\n" name);
 		c("#include <avr/io.h>");
@@ -18,20 +19,40 @@ let rec gen t =
 		c("void setup(){");
 
         gen_actuators(actuators);
-        gen_actions(actions);
+        gen_states(states);
+        
+		c("int main(void) {");
+		c("  setup();");
+		c(Printf.sprintf "  state_%s();" initial.name);
+		c("  return 0;");
+		c("}");
 
-and gen_actuators = function 
+and gen_actuators : actuators -> unit = function 
     | [] -> ()
     | act :: acts -> gen_actuator act ; gen_actuators acts   
 
-and gen_actuator act = 
-    c(Printf.sprintf "  pinMode(%d, OUTPUT); // %s [Actuator]" (snd act) (fst act));
+and gen_actuator : actuator -> unit = fun actuator ->
+    c(Printf.sprintf "  pinMode(%d, OUTPUT); // %s [Actuator]" actuator.pin actuator.name)
 
-and gen_actions = function 
+and gen_states : states -> unit = function
+    | [] -> ()
+    | s :: states -> gen_state s ; gen_states states
+
+and gen_state : state -> unit = fun s ->
+        c(Printf.sprintf "void state_%s() {" s.name);
+        gen_actions(s.actions);
+		c("  _delay_ms(1000);");
+		c(Printf.sprintf "  state_%s();" s.next.name);
+		c("}");
+
+and gen_actions : actions -> unit = function 
     | [] -> ()
     | action :: actions -> gen_action action ; gen_actions actions 
 
-and gen_actions action =
-    c(Prinft.sprintf("  digitalWrite(%d,%s);",action.getActuator().getPin(),action.getValue()));
+and gen_action : action -> unit = fun action ->
+    c(Printf.sprintf "  digitalWrite(%d,%d);" action.actuator.pin (int_of_signal(action.value)))
 
+and int_of_signal : signal -> int = function 
+    | Low -> 0
+    | High -> 1
 

8a23b93418599c7b1f7bdd50c32e9a2a41e07d72
gduboc <guillaume.duboc@ens-lyon.fr>
2018-10-09 22:48:13 +0200

    step5 Simplify code architecture

--- lab_1/_code/step5/src/main/java/io/github/mosser/arduinoml/ens/generator/Visitable.java
+++
@@ -1,7 +0,0 @@
-package io.github.mosser.arduinoml.ens.generator;
-
-public interface Visitable {
-
-	void accept(Visitor visitor);
-
-}
--- lab_1/_code/step5/src/main/java/io/github/mosser/arduinoml/ens/model/NamedElement.java
+++
@@ -1,9 +0,0 @@
-package io.github.mosser.arduinoml.ens.model;
-
-
-public interface NamedElement {
-
-	public void setName(String name);
-	public String getName();
-
-}
